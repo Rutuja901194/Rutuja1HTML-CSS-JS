c710c4be53f7bbc418fdf13c78cb7aa3cec859a0
Rutuja901194 <rutujasapkal9011@gmail.com>
2025-04-05 16:52:59 +0530

    index.php
    for connection used the action.php file

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>                                                                                                                                                                                          <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="action.php" method="post">
+        <div class="container">
+        <div class="row ">
+            <div class=" col-sm-12">
+            <h3 class="gap mt-4 border">form1</h3>
+            </div>
+        <div class="row">
+          <div class="col-sm-4">
+           <label>Name</label>
+            <input type="name" class="form-control" name="name" aria-describedby="Name">
+            <br> 
+         </div>
+         </div>
+         <br>
+         <div class="col-sm-4">
+            <label>contact No: </label>
+             <input type="contact" class="form-control" name="contact" aria-describedby="contact">
+          </div>
+          <br>
+          <div class="col-sm-4">
+            <label>City:</label>
+             <input type="city" class="form-control" name="city" aria-describedby="city">
+          </div>
+          <br>
+          <div class="col-sm-4">
+          <button class="submit"> submit</button>
+    
+        </div>
+        
+        </div>
+      </form>
+</body>
+</html>
+                                                                                                                                                                                                         <?php
+
+
+//action.php
